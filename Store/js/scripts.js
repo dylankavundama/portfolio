f266ec5e5caa036957d9c57dce878e11ee6b10e0
Dylan Kavundama <dylancorporation.cd@gmail.com>
2025-09-15 18:27:39 +0200

    go

--- Store/js/scripts.js
+++ Store/js/scripts.js
@@ -19,7 +19,7 @@ document.addEventListener('DOMContentLoaded', function() {
                     </div>
                     <div class="card-footer p-4 pt-0 border-top-0 bg-transparent">
                         <div class="text-center">
-                            <a class="btn btn-outline-dark mt-auto app-details-btn" href="#${appId}" data-bs-toggle="modal" data-bs-target="#appDetailsModal">Voir les détails</a>
+                            <a class="btn btn-outline-dark mt-auto app-details-btn" href="#${appId}" data-bs-toggle="modal" data-bs-target="#appDetailsModal">Voir plus</a>
                         </div>
                     </div>
                 </div>
